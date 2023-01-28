8a3c44ee71ce232054b43464094afa1956137d3e
MichaelSalSail <msalamo000@citymail.cuny.edu>
2023-01-27 19:13:13 -0500

    Retrieve file metadata: file name & word count

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,10 +9,35 @@ import {
 export default function Dashboard() {
     // Track whether the user uploaded a resume or not
     const [resume, setResume] = useState(false);
-    // Simple function called upon file upload
-    const onFileChange = () => {
+    // Store resume metadata
+    const [filename, setFileName] = useState("");
+    const [wordcount, setWordCount] = useState(0);
+    // function called upon file upload
+    const onFileChange = (data) => {
+        let reader = new FileReader();
+        try
+        {
+            console.log("File Name:",data.target.files[0]['name']);
+            setFileName(data.target.files[0]['name']);
+            reader.readAsText(data.target.files[0]);
+            reader.onload = function() {
+                console.log(reader.result);
+                let temp = reader.result;
+                const arr = temp.split(' ');
+                let total=arr.filter(word => word !== '').length;
+                console.log("Word Count:", total);
+                setWordCount(total);
+            };
+        }
+        catch(error)
+        {
+            console.log("Failed to select file!")
+        }
+        // We now have a resume!
         if(resume===false)
             setResume(true)
+        // Reset parse if file uploaded before
+        setParse(false);
     }
     // Parse the resume upon button click
     const [parse, setParse] = useState(false);
@@ -60,7 +85,7 @@ export default function Dashboard() {
                     }}
                 >
                     <br></br>
-                    <Typography color="white" align="center">File Name: {parse ? (example_values["file_name"]):(default_values["file_name"])}</Typography>
+                    <Typography color="white" align="center">File Name: {parse ? (filename):(default_values["file_name"])}</Typography>
                     <br></br>
                     <Typography color="white" align="center"># of Pages: {parse ? (example_values["pages"]):(default_values["pages"])}</Typography>
                     <br></br>
@@ -68,7 +93,7 @@ export default function Dashboard() {
                     <br></br>
                     <Typography color="white" align="center">Bullet Points: {parse ? (example_values["bullet_pts"]):(default_values["bullet_pts"])}</Typography>
                     <br></br>
-                    <Typography color="white" align="center">Total Words: {parse ? (example_values["word_count"]):(default_values["word_count"])}</Typography>
+                    <Typography color="white" align="center">Total Words: {parse ? (wordcount):(default_values["word_count"])}</Typography>
                 </Box>
             </Container>
         </form>
